5424107201dabd9a45fbafe41ee0ba2a3d670c7d
Colton <kaiserc2@seattleu.edu>
2020-06-27 16:44:12 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,62 @@ import ToyContainer from './components/ToyContainer'
 
 import data from './data'
 
+const API = 'http://localhost:3000/toys'
+
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
+  }
+
+  handlePost = (toy) => {
+    let currentToys = this.state.toys
+    let obj = {
+      name: toy.name,
+      image: toy.image
+    }
+    fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'},
+      body: JSON.stringify(obj)
+    })
+    currentToys.push(obj)
+  }
+
+  handleDelete = (toyCard) => {
+    fetch(`${API}/${toyCard.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'}
+      }
+    )
+  }
+
+  handleLikes = (toyCard) => {
+    let obj = {
+      likes: toyCard.likes + 1
+    }
+    fetch(`${API}/${toyCard.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'},
+        body: JSON.stringify(obj)
+      }
+    )
+  }
+
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        toys: data
+      })
+    })
   }
 
   handleClick = () => {
@@ -27,14 +78,15 @@ class App extends React.Component{
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm toys={this.state.toys} handlePost={this.handlePost}
+          handleSubmit={this.handleSubmit}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer handleLikes={this.handleLikes} handleDelete={this.handleDelete} toys={this.state.toys}/>
       </>
     );
   }
