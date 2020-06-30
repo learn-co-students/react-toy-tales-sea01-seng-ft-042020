eb6683807fa72631be4c44b1a62199cf2adf2c1c
Joshua Mclean <joshmcleana@gmail.com>
2020-06-29 19:25:42 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,107 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import ToyForm from "./components/ToyForm";
+import ToyContainer from "./components/ToyContainer";
+const API = "  http://localhost:3000/toys";
 
-import Header from './components/Header'
-import ToyForm from './components/ToyForm'
-import ToyContainer from './components/ToyContainer'
+// import data from './data'
 
-import data from './data'
-
-
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-    display: false
+    display: false, //display the form or not
+    toys: [],
+  };
+
+  fetchToys = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((json) =>
+        this.setState({
+          toys: json,
+        })
+      );
+  };
+
+  componentDidMount() {
+    this.fetchToys();
   }
 
   handleClick = () => {
-    let newBoolean = !this.state.display
+    let newBoolean = !this.state.display;
     this.setState({
-      display: newBoolean
-    })
-  }
+      display: newBoolean,
+    });
+  };
+  addToy = (toy) => {
+    toy.likes = 0;
+    let configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    };
+    fetch(API, configObject)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          toys: [...this.state.toys, json],
+        });
+      });
+  };
 
-  render(){
+  handleDonate = (e, toy) => {
+    console.log(toy)
+    let configObject = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    };
+    fetch(`${API}/${toy.id}`, configObject)
+      .then((res) => res.json())
+      .then(
+        this.setState({
+          toys: [...this.state.toys.filter((aToy) => aToy.id !== toy.id)],
+        })
+      );
+  };
+
+  handleLike = (e, toy) => {
+    toy.likes++;
+    console.log(toy);
+    let configObject = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    };
+    fetch(`${API}/${toy.id}`, configObject)
+      .then((res) => res.json())
+      .then(this.fetchToys());
+  };
+
+  render() {
+    console.log(this.state);
     return (
       <>
-        <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        <Header />
+        {this.state.display ? <ToyForm addToy={this.addToy} /> : null}
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer
+          toys={this.state.toys}
+          handleDonate={this.handleDonate}
+          handleLike={this.handleLike}
+        />
       </>
     );
   }
-
 }
 
 export default App;
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 class ToyCard extends Component {
 
   render() {
+    const {image, likes, name} = this.props.toy
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{name}</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
+        <button onClick={(e)=> this.props.handleLike(e, this.props.toy)} className="like-btn">Like {'<3'}</button>
+        <button onClick={(e)=> this.props.handleDonate(e, this.props.toy)} className="del-btn">Donate to GoodWill</button>
       </div>
     );
   }
